77e1ee6b380a1e3873b058893cf0904fe246b02e
Oakley Nieters <87721373+OakleyNieters@users.noreply.github.com>
2021-10-05 14:18:25 -0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -5,6 +5,8 @@ import (
 	"movieproj/entities"
 	"movieproj/service"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type MovieHandler struct {
@@ -41,25 +43,41 @@ func (mh MovieHandler) PostNewMovie(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
-func (mh MovieHandler) getMovies(w http.ResponseWriter, r *http.Request) {
-	mv := entities.Movies{}
+func (mh MovieHandler) GetMovieHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["Id"]
 
-	err := json.NewDecoder(r.Body).Decode(&mv)
+	mvID, err := mh.Svc.FindById(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+
+	movie, err := json.MarshalIndent(mvID, "", " ")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
-	err = mh.Svc.CreateNewMovie(mv)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(movie)
+}
+
+func (mh MovieHandler) DeleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["Id"]
+
+	mvID, err := mh.Svc.FindById(id)
 	if err != nil {
-		switch err.Error() {
-		case "invalid movie":
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		case "movie does not exist":
-			http.Error(w, err.Error(), http.StatusNotFound)
-
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-
+		http.Error(w, err.Error(), http.StatusNotFound)
 	}
+
+	movie, err := json.MarshalIndent(mvID, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(movie)
+	json.NewEncoder(w).Encode(movie)
 }
