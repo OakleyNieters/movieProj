e0374e0b805b0607222855ef8a6c38f8dfb5c725
Oakley Nieters <87721373+OakleyNieters@users.noreply.github.com>
2021-10-07 20:27:42 -0500

    Update movie.go

--- movie.go
+++ movie.go
@@ -1,11 +1,17 @@
 package entities
 
+import "github.com/google/uuid"
+
 type Movies struct {
-	ID          string
-	Title       string
-	Genre       []string
-	Description string
-	Director    string
-	Actors      []string
-	Rating      int
+	ID          string   `json:"ID"`
+	Title       string   `json:"Title"`
+	Genre       []string `json:"Genre"`
+	Description string   `json:"Description"`
+	Director    string   `json:"Director"`
+	Actors      []string `json:"Actors"`
+	Rating      float32  `json:"Rating"`
+}
+
+func (m *Movies) SetId() {
+	m.ID = uuid.New().String()
 }
